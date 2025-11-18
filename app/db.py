8f9a0c53034dce47452b244c21c2b91a063c4ceb
597367462@qq.com <597367462@qq.com>
2025-11-18 17:21:08 +0000

    login and signup

--- app/db.py
+++ app/db.py
@@ -7,10 +7,10 @@ load_dotenv()  # 默认会加载项目根目录下的 .env 文件
 DB_HOST = os.getenv("DB_HOST")
 DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
-DB_PASS = os.getenv("DB_PASS")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_PORT = os.getenv("DB_PORT")
 
-DATABASE_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-
+DATABASE_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+print(DATABASE_URL)
 engine = create_engine(DATABASE_URL)
 
--- main.py
+++ main.py
@@ -5,9 +5,12 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI, Depends, HTTPException, status
 from pydantic import BaseModel
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from app.auth import verify_password, create_access_token
+from app.auth import verify_password, create_access_token, get_password_hash
 from sqlalchemy import text
 from app.db import engine
+
+import uuid
+import datetime
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -26,8 +29,13 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
-class UserLoginModel(BaseModel):
+class UserSignupModel(BaseModel):
+    email: str
     username: str
+    password: str
+    
+class UserLoginModel(BaseModel):
+    email: str
     password: str
     
 # 模拟用户数据
@@ -48,22 +56,52 @@ def read_root():
         return {"message": "Database connected!", "data": rows}
     except Exception as e:
             return {"error": str(e)}
+        
+@app.post("/signup")
+def login_user(user: UserSignupModel):
+    try:
+        with engine.connect() as conn:
+            # 生成 UUID
+            user_uuid = str(uuid.uuid4())
+            
+            # 构建 SQL 语句
+            sql = text("""
+                INSERT INTO t_user (uuid, username, password, email, del_flag) 
+                VALUES (:uuid, :username, :password, :email, :del_flag)
+            """)
+            # 执行插入操作
+            result = conn.execute(sql, {
+                'uuid': user_uuid,
+                'username': user.username,
+                'password': get_password_hash(user.password),  # 注意：密码应该先加密再存储
 
-@app.get("/login")
-def login():
-    return {"message": "Login endpoint"}
+                'email': user.email,
+                'del_flag': False  # 默认值，表示未删除
+            })
+            
+            # 提交事务
+            conn.commit()
+            
+        return {"message": "User registered successfully!", "user_id": user_uuid}
+    except Exception as e:
+            return {"error": str(e)}
 
 @app.post("/login")
 def login_user(user: UserLoginModel):
-    return {"username": user.username, "password": user.password}
-
-@app.post("/token")
-def login(form_data: OAuth2PasswordRequestForm = Depends()):
-    user = fake_users_db.get(form_data.username)
-    if not user or not verify_password(form_data.password, user["hashed_password"]):
-        raise HTTPException(status_code=400, detail="Incorrect username or password")
-    access_token = create_access_token(data={"sub": user["username"]})
-    return {"access_token": access_token, "token_type": "bearer"}
+    try:
+        with engine.connect() as conn:
+            sql = text("select * from t_user t where t.email = :email")
+            result = conn.execute(sql, { "email": user.email })
+            rows = [dict(row._mapping) for row in result]
+            if len(rows) <= 0:
+                raise HTTPException(status_code=400, detail="Incorrect username or password")
+            if not verify_password(user.password, rows[0].get('password')):
+                raise HTTPException(status_code=400, detail="Incorrect username or password")
+            
+        access_token = create_access_token(data={"sub": rows[0].get('email')})
+        return {"token": access_token, "prefix": "Bearer"}
+    except Exception as e:
+            return {"error": str(e)}
 
 @app.get("/users/me")
 def read_users_me(token: str = Depends(OAuth2PasswordBearer(tokenUrl="token"))):
